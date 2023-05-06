fa420278ae73063863310c260d29f18d4cf7d103
Nikita Kupriyashin <kupriyashin.nick@yandex.ru>
2023-05-06 10:53:33 +0300

    Перешел на Tkinter

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def geometry_center():
     x_cordinate = int((screen_width / 2) - (window_width / 2))
     y_cordinate = int((screen_height / 2) - (window_height / 2))
 
-    root.geometry("{}x{}+{}+{}".format(window_width, window_height, x_cordinate, y_cordinate))
+    root.geometry(f"{window_width}x{window_height}+{x_cordinate}+{y_cordinate}")
 
 
 logger.add("debug.log", format="{time} {level} {message}", level="DEBUG")
